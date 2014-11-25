7cc77aeb040430cbc19938b3dc747ab121db0d88
Dmitry Zv <dmitryzv@hotmail.com>
2014-11-25 00:01:30 -0800

    Fixed Nona capitalization in error handlers

--- application.py
+++ application.py
@@ -2,18 +2,17 @@ import os
 import math
 
 import flask
-from flask import request, Response
 from flask import Flask, jsonify, send_file, Response, request
 
 # Create the Flask app
 app = flask.Flask(__name__)
 
 @app.errorhandler(400)
-def bad_request(e=none):
+def bad_request(e=None):
 	return jsonify({'error': 'bad request'}), 400
 
 @app.errorhandler(404)
-def not_found(e=none):
+def not_found(e=None):
 	return jsonify({'error': 'resource not found'}), 404
 
 @app.errorhandler(405)
@@ -21,7 +20,7 @@ def method_not_supported(e):
 	return jsonify({'error': 'method not supported'}), 405
 
 @app.errorhandler(500)
-def internal_server_error(e=none):
+def internal_server_error(e=None):
 	return jsonify({'error': 'internal server error'}), 500
 
 @app.route('/api/fibonacci/<int:num>', methods=['GET'])
