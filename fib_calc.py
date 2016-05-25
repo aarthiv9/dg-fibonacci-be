8385fd6ff6460cb517fb04cc9f42ab06f70d4589
dmitryzv <dmitry.zviagintsev@digitalglobe.com>
2016-05-25 17:03:25 -0600

    Update fib_calc.py

--- fib_calc.py
+++ fib_calc.py
@@ -4,7 +4,7 @@
 def _fib2(n):
     if n == 0: return (0, 1)
     half_n, is_n_odd = divmod(n, 2)
-    f_n, f_n_plus_1 = fib2(half_n)
+    f_n, f_n_plus_1 = _fib2(half_n)
     f_n_squared = f_n * f_n
     f_n_plus_1_squared = f_n_plus_1 * f_n_plus_1
     f_2n = 2 * f_n * f_n_plus_1 - f_n_squared
@@ -14,4 +14,4 @@ def _fib2(n):
     return (f_2n, f_2n_plus_1)
 
 def fib(n):
-    return fib2(n)[0]
+    return _fib2(n)[0]
